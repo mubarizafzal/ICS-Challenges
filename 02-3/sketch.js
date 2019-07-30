aa42cbc9a8136654090762763d8026176909bb5d
Mubariz Afzal <mubarizafzal@gmail.com>
2019-07-30 19:33:04 -0400

    small update

--- 02-3/sketch.js
+++ 02-3/sketch.js
@@ -1,7 +1,8 @@
 /**
- * Problem: 
+ * Problem: Create a program that fills the screen with randomly sized rectangles that do not overlap,
+ * but fill the screen. The maximum size of a rectangle is 1/15th of the screen size.
  * 
- * Notes: 
+ * Notes: Instead of maximum size, the minimum size is 1/32 of the canvas and the maximum is 1/4 of the screen. Squares instead of rectangles.
  * 
  * 
  */
@@ -12,5 +13,26 @@ function setup () {
 }
 
 function draw () {
+  noLoop();
+  recursive(0,0,300,300);
+}
 
+function recursive (x, y, w, h) {
+
+  if (w <= 9 || h <= 9) {
+    return;
+  }
+  
+  if (random() <= 0.25) {
+    return;
+  } else {
+
+    line (x + w/2, y, x + w/2, y + h);
+    line (x, y + h/2, x + w, y + h/2);
+
+    recursive (x, y, w/2, h/2);
+    recursive (x + w/2, y, w/2, h/2);
+    recursive (x, y + h/2, w/2, h/2);
+    recursive (x + w/2, y + h/2, w/2, h/2);
+  }
 }
--- 02-4/sketch.js
+++ 02-4/sketch.js
@@ -1,5 +1,6 @@
 /**
- * Problem: 
+ * Problem: Create a program that draws between 80 and 120 points (randomly chosen) to the screen.
+ * Have the program connect the dots using lines; however, you can only generate non-overlapping triangles.
  * 
  * Notes: 
  * 
@@ -12,5 +13,63 @@ function setup () {
 }
 
 function draw () {
+  noLoop();
+  triangles(300,300);
+
+}
+
+function arrDiff (value, index, arr) {
+  arr[index] = value - this;
+}
+
+function triangles (w, h) {
+
+  let Max = floor(random(8,12));
+  let arrX = [];
+  let arrY = [];
+
+  strokeWeight(3);
+  for (let i = 0; i < Max; i++) {
+    let ranX = floor(random(0,w+1));
+    let ranY = floor(random(0,h+1));
+    
+    point(ranX, ranY);
+    arrX.push(ranX);
+    arrY.push(ranY);
+  }
+  console.log(arrX);
+  // comeback to itself in 2 points, no points within area of triangle
+  // find nearest point
+  // quadtree
+
+  // create by nearest
+  while (arrX.length > 0) {
+    // take a point
+    let x = arrX[0];
+    let y = arrY[0];
+
+    let tempX = arrX.slice(1);
+    let tempY = arrY.slice(1);
+    // find a nearest
+
+    // diff of x values in arr, diff of y values in arr, add x arr and y arr, find pos of smallest val
+    tempX.forEach(arrDiff,x);
+
+
+    // connect to another nearest
+
+    // connect back
+
+    // remove 3 from arr - actually no
+
+    // creat triangle obj
+
+
+  }
+
+
+
+
+
 
 }
