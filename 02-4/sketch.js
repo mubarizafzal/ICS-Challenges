7a8471439f19ab1dd98e6e9e7a4d8a533e0ba3db
Mubariz Afzal <mubarizafzal@gmail.com>
2019-08-03 15:00:05 -0400

    needs debugging

--- 02-4/sketch.js
+++ 02-4/sketch.js
@@ -14,13 +14,44 @@ function setup () {
 
 function draw () {
   noLoop();
+  console.log("something");
   triangles(300,300);
 
 }
 
-function arrDiff (value, index, arr) {
-  arr[index] = value - this;
+function xCollide (val, y, h, arr) {
+  if (arr.length == 0) {
+    return false;
+  }
+
+  for (let arrs of arr) {
+    if (arrs[0] == val) {
+      if (y <= (arrs[1] + arrs[3]) || (y + h) >= arrs[1]) {
+        return true;
+      } 
+    }
+
+  }
+  return false;
 }
+
+
+function yCollide (val, x, w, arr) {
+  if (arr.length == 0) {
+    return false;
+  }
+
+  for (let arrs of arr) {
+    if (arrs[1] == val) {
+      if (x <= (arrs[0] + arrs[2]) || (x + w) >= arrs[0]) {
+        return true;
+      } 
+    }
+  }
+  return false;
+
+}
+
 
 function triangles (w, h) {
 
@@ -38,33 +69,113 @@ function triangles (w, h) {
     arrY.push(ranY);
   }
   console.log(arrX);
-  // comeback to itself in 2 points, no points within area of triangle
-  // find nearest point
-  // quadtree
 
-  // create by nearest
-  while (arrX.length > 0) {
-    // take a point
-    let x = arrX[0];
-    let y = arrY[0];
+  let grows = [[0,0]];
+  let squares = []; // [[x,y,w,h]] top left
+  strokeWeight(1);
+  noFill();
 
-    let tempX = arrX.slice(1);
-    let tempY = arrY.slice(1);
-    // find a nearest
+  console.log("yes");
+  while (grows.length > 0) {
+    let x = grows[0][0];
+    let y = grows[0][1];
+    let rectW = 0;
+    let rectH = 0;
 
-    // diff of x values in arr, diff of y values in arr, add x arr and y arr, find pos of smallest val
-    tempX.forEach(arrDiff,x);
+    let xHit = false;
+    let xCollided = false;
+    let yCollided = false;
+    let yHit = false;
+
+    while (xHit == false || yHit == false) {
+
+      if (xHit == false) {
+        rectW++;
+      } else {
+        if (rectW == 1) {
+          return;
+        }
+      }
 
 
-    // connect to another nearest
+      if (yHit == false) {
+        rectH++;
 
-    // connect back
+        for (let pY of arrY) {
+          if (pY == y) {
+            let index = arrY.indexOf(pY);
+            let pX = arrX[index];
+            if (pX >= x && pX <= (x + rectW)){
+              yHit = true;
+            }
+          }
+        }
+      } else {
+        if (rectH == 1) {
+          return;
+        }
+      }
 
-    // remove 3 from arr - actually no
+      if (xHit == false) {
+        for (let pX of arrX) {
+          if (pX == x) {
+            let index = arrX.indexOf(pX);
+            let pY = arrY[index];
+            if (pY >= y && pY <= (y + rectH)){
+              xHit = true;
+            }
+          }
+        }
+      }
 
-    // creat triangle obj
+      if (xHit == false) {
+        if(xCollide(x + rectW, y, rectH, squares)) {
+          xHit = true;
+          xCollided = true;
+        }
+      }
 
+      if (yHit == false) {
+        if(yCollide(y + rectH, x, rectW, squares)) {
+          yHit = true;
+          yCollided = true;
+        }
+      }
 
+      if ((x + rectW) == (w - 1)) {
+        xHit = true;
+      }
+
+      if ((y + rectH) == (h - 1)) {
+        yHit == true;
+      }
+
+    }
+
+    // now add the right and left points to grow
+    grows.shift();
+    console.log("length is " + grows.length);
+
+    if (rectH != 1 && rectW != 1) {
+      console.log(x,y,rectW,rectH);
+      
+      //rectH(x,y,rectW,rectH);
+      
+      if (!xCollided) {
+        if ((x + rectW) < (w - 1)) {
+          grows.push([x + rectW + 1, y]);
+        }
+      }
+
+      if (!yCollided) {
+        if ((y + rectH) < (h -1)) {
+          grows.push([x, y + rectH + 1])
+        }
+      }
+      
+
+      
+    }
   }
 
 
