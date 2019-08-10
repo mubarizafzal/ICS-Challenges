6a80ba9eadfa2f54caa6878c3611521bbd32f6e9
Mubariz Afzal <mubarizafzal@gmail.com>
2019-08-10 18:21:38 -0400

    small update

--- 04-1/sketch.js
+++ 04-1/sketch.js
@@ -10,11 +10,13 @@
 
 
 // a hundred random points
+// not working properlly
 
 function setup () {
   createCanvas(500,500);
   pixelDensity(1);
-  rectMode(CORNERS);
+  rectMode(CENTER);
+  colorMode(RGB, 255);
   stroke(2);
   textSize(20);
 
@@ -23,5 +25,56 @@ function setup () {
 
 function draw () {
   background(255);
+  noLoop();
+
+  strokeWeight(1);
+  let MAX = 100;
+  let arrX = [];
+  let arrY = [];
+  let arrR = [];
+
+  for (let i = 0; i < MAX; i++) {
+    let ranX = Math.floor(Math.random()*width);
+    let ranY = Math.floor(Math.random()*height);
+    
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    
+    fill(r, g, b);
+    circle(ranX, ranY, 30);
+
+    if (arrX.includes(ranX)) {
+      let index = arrX.indexOf(ranX);
+      if (arrY[index] == ranY) {
+        // same cords
+        arrR[index] = r;
+
+      } else {
+        arrR.push(r);
+        arrX.push(ranX);
+        arrY.push(ranY);
+      }
+    } else {
+      arrR.push(r);
+      arrX.push(ranX);
+      arrY.push(ranY);
+    }
+  }
+
+  noFill();
+  for (let i = 0; i < arrX.length; i++) {
+    if (arrR[i] >= 200) {
+      let x = arrX[i];
+      let y = arrY[i];
+
+      rect(x, y, 30, 30);
+    }
+  }
 
 }
+
+
+
+
+
